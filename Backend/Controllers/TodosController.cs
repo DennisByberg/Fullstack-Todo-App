9bbc9a89e2768bdea594a8822520675f67efb486
Dennis Byberg <dennis.byberg@hotmail.com>
2024-08-21 21:43:34 +0200

    refactoring get controller

--- Backend/Controllers/TodosController.cs
+++ Backend/Controllers/TodosController.cs
@@ -5,12 +5,16 @@ namespace Backend.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class TodosController(ApplicationDbContext dbContext) : ControllerBase
+    public class TodosController : ControllerBase
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public TodosController(ApplicationDbContext dbContext) => _dbContext = dbContext;
+
         [HttpGet]
         public IActionResult GetAllTodos()
         {
-            return Ok(dbContext.Todos.ToList());
+            return Ok(_dbContext.Todos.ToList());
         }
     }
 }
