49c51c14ca3643ecb9bc34bddac0eacc899540e2
Dennis Byberg <dennis.byberg@hotmail.com>
2024-08-21 21:31:58 +0200

    added new controller to get all the todos

---
+++ Backend/Controllers/TodosController.cs
@@ -0,0 +1,16 @@
+﻿using Backend.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodosController(ApplicationDbContext dbContext) : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllTodos()
+        {
+            return Ok(dbContext.Todos.ToList());
+        }
+    }
+}
